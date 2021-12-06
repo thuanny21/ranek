22c68bb117f29d9991859704e7b59e9b131faca5
Thuanny <thucatm@gmail.com>
2021-12-06 13:16:48 -0300

    start projeto

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style>
+
+</style>
