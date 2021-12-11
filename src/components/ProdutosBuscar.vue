1fa07c629d078ccbca430250613b64c3eed922f8
Thuanny <thucatm@gmail.com>
2021-12-11 17:31:39 -0300

    estilo css do input - buscar

--- src/components/ProdutosBuscar.vue
+++ src/components/ProdutosBuscar.vue
@@ -20,5 +20,35 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+form {
+  max-width: 600px;
+  margin: 30px auto 60px auto;
+  position: relative;
+}
+
+#busca {
+  width: 100%;
+  padding: 20px;
+  border: none;
+}
+
+#busca:focus,
+#busca:hover {
+  transform: scale(1.1);
+}
+
+#lupa {
+  width: 62px;
+  height: 62px;
+  background: url("../assets/search.svg") no-repeat center center;
+  text-indent: -150px;
+  border: none;
+  cursor: pointer;
+  position: absolute;
+  top: 0px;
+  right: 0px;
+  box-shadow: none;
+}
 </style>
+
