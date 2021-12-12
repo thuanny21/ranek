1b69d0c9b8dde2d78a4b9220245280162111491d
Thuanny <thucatm@gmail.com>
2021-12-12 17:13:48 -0300

    efeito transição

--- src/components/ProdutosLista.vue
+++ src/components/ProdutosLista.vue
@@ -1,19 +1,22 @@
 <template>
     <section class="produtos-container">
-        <div v-if="produtos && produtos.length" class="produtos">
-            <div class="produto" v-for="(produto, index) in produtos" :key="index">
-                <router-link to="/">
-                    <img v-if="produto.fotos" :src="produto.fotos[0].src" :alt="produto.fotos[0].titulo">
-                    <p class="preco">{{produto.preco}}</p>
-                    <h2 class="titulo">{{produto.nome}}</h2>
-                    <p>{{produto.descricao}}</p>
-                </router-link>
+        <transition mode="out-in">
+            <div v-if="produtos && produtos.length" class="produtos" key="produtos">
+                <div class="produto" v-for="(produto, index) in produtos" :key="index">
+                    <router-link to="/">
+                        <img v-if="produto.fotos" :src="produto.fotos[0].src" :alt="produto.fotos[0].titulo">
+                        <p class="preco">{{produto.preco}}</p>
+                        <h2 class="titulo">{{produto.nome}}</h2>
+                        <p>{{produto.descricao}}</p>
+                    </router-link>
+                </div>
+                <ProdutosPaginar :produtosTotal="produtosTotal" :produtosPorPagina="produtosPorPagina"/>
             </div>
-            <ProdutosPaginar :produtosTotal="produtosTotal" :produtosPorPagina="produtosPorPagina"/>
-        </div>
-        <div v-else-if="produtos && produtos.length === 0">
-            <p class="sem-resultados">Busca sem resultados. Tente buscar outro termo.</p>
-        </div>
+            <div v-else-if="produtos && produtos.length === 0" key="sem-resultados">
+                <p class="sem-resultados">Busca sem resultados. Tente buscar outro termo.</p>
+            </div>            
+            <PaginaCarregando key="carregando" v-else/>           
+        </transition>
     </section>
 </template>
 
@@ -30,7 +33,7 @@ export default {
     data() {
         return {
             produtos: null,
-            produtosPorPagina: 3,
+            produtosPorPagina: 9,
             produtosTotal: 0,            
         };        
     },
@@ -42,6 +45,7 @@ export default {
     },
     methods: {
         getProdutos() {
+            this.produtos = null;
             api.get(this.url).then(response => {
                 this.produtosTotal = Number(response.headers["x-total-count"]);
                 this.produtos = response.data;
--- src/router/index.js
+++ src/router/index.js
@@ -2,21 +2,20 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 
+
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
- 
-]
-
-const router = new VueRouter({
-  mode: 'history',
+export default new VueRouter({
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
-
-export default router
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: Home
+    }
+  ],
+  scrollBehavior() {
+    return window.scrollTo({ top: 0, behavior: "smooth" });
+  }
+});
