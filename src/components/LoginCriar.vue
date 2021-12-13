435564ca59c8334b8dc8ae0b6b050273dfed1062
Thuanny <thucatm@gmail.com>
2021-12-13 11:59:00 -0300

    post usuario - criar usuario

--- src/components/LoginCriar.vue
+++ src/components/LoginCriar.vue
@@ -4,7 +4,7 @@
         <transition mode="out-in">
         <button v-if="!criar" class="btn criar" @click="criar = true">Criar Conta</button>
         <UsuarioForm v-else>
-            <button class="btn btn-form">Criar Usuario</button>
+            <button class="btn btn-form" @click.prevent="criarUsuario">Criar Usuario</button>
         </UsuarioForm>
         </transition>
     </section>
@@ -21,6 +21,17 @@ export default {
     data() {
         return {
             criar: false
+        }
+    },
+    methods: {
+        async criarUsuario() {
+            try {
+                await this.$store.dispatch("criarUsuario", this.$store.state.usuario);
+                await this.$store.dispatch("getUsuario", this.$store.state.usuario.email);
+                this.$router.push({ name: "usuario" });
+            } catch (error) {
+                console.log(error);
+            }
         }
     }
 }
