73071449e9c543f3720b76762c949f939596fcc9
Thuanny <thucatm@gmail.com>
2021-12-18 17:09:28 -0300

    função deletar produto e estilo btn do deletar

--- src/views/usuario/UsuarioProdutos.vue
+++ src/views/usuario/UsuarioProdutos.vue
@@ -7,6 +7,7 @@
         <li v-for="(produto, index) in usuario_produtos" :key="index">
           <ProdutoItem :produto="produto">  
             <p>{{produto.descricao}}</p>
+            <button class="deletar" @click="deletarProduto(produto.id)">Deletar</button>
           </ProdutoItem>       
         </li>
       </transition-group>
@@ -16,7 +17,8 @@
 <script>
 import ProdutoAdicionar from '@/components/ProdutoAdicionar.vue';
 import ProdutoItem from '@/components/ProdutoItem.vue';
-import { mapState, mapActions } from 'vuex'
+import { mapState, mapActions } from 'vuex';
+import { api } from '@/services.js';
 
 
 export default {
@@ -30,7 +32,20 @@ export default {
       ...mapState(["login", "usuario", 'usuario_produtos'])
     },
     methods: {
-      ...mapActions(["getUsuarioProdutos"])
+      ...mapActions(["getUsuarioProdutos"]),
+      deletarProduto(id) {
+        const confirmar = window.confirm("Deseka remover este produto?");
+        if(confirmar){
+          api
+          .delete(`/produto/${id}`)
+          .then(() => {
+            this.getUsuarioProdutos();            
+          })
+          .catch(error => {
+            console.log(error.response);
+          });
+        }    
+      }
     },
     watch: {
       login() {
@@ -59,5 +74,18 @@ h2 {
 .list-enter-active,
 .list-leave-active {
   transition: all 0.3s;
+}
+
+.deletar{
+  position: absolute;
+  top: 0px;
+  right: 0px;
+  background: url("../../assets/remove.svg") no-repeat center center;
+  width: 24px;
+  height: 24px;
+  text-indent: -140px;
+  overflow: hidden;
+  cursor: pointer;
+  border: none;
 }
 </style>
