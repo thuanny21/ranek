a5634bb7668589968911f878b5d64d846a9c3ea6
Thuanny <thucatm@gmail.com>
2021-12-18 16:55:51 -0300

    adicionar produto, metodos e estilos

--- src/views/usuario/UsuarioProdutos.vue
+++ src/views/usuario/UsuarioProdutos.vue
@@ -46,5 +46,18 @@ export default {
 </script>
 
 <style scoped>
+h2 {
+  margin-bottom: 20px;
+}
 
+.list-enter,
+.list-leave-to {
+  opacity: 0;
+  transform: translate3d(20px, 0, 0);
+}
+
+.list-enter-active,
+.list-leave-active {
+  transition: all 0.3s;
+}
 </style>
